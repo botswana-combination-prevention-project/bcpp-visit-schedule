056b6650f6ad47ba10d6d4eac14ff7f9aa7e5251
erikvw <ew2789@gmail.com>
2017-09-07 09:33:56 +0200

    venous = 'Venous (HIV)'

--- bcpp_visit_schedule/visit_schedule/requisitions.py
+++ bcpp_visit_schedule/visit_schedule/requisitions.py
@@ -4,7 +4,7 @@ RESEARCH_BLOOD_DRAW = 'Research Blood Draw'
 MICROTUBE = 'Microtube'
 VIRAL_LOAD = 'Viral Load'
 ELISA = 'ELISA'
-VENOUS = 'Venous'
+VENOUS = 'Venous (HIV)'
 
 
 elisa_panel = Panel(name=ELISA)
